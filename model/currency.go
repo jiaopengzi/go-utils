641c19768d06a9914d72481438e83a5e0e490d58
jiaopengzi <jiaopengzi@outlook.com>
2026-01-19 12:23:24 +0800

    add currency

---
+++ model/currency.go
@@ -0,0 +1,35 @@
+//
+// FilePath    : go-utils\model\currency.go
+// Author      : jiaopengzi
+// Blog        : https://jiaopengzi.com
+// Copyright   : Copyright (c) 2026 by jiaopengzi, All Rights Reserved.
+// Description : 货币类型
+//
+
+package model
+
+type Currency int // 订单货币类型
+
+// 定义订单货币类型常量
+const (
+	CurrencyCNY Currency = iota + 1 // 1 人民币
+	CurrencyUSD                     // 2 美元
+	CurrencyEUR                     // 3 欧元
+	CurrencyGBP                     // 4 英镑
+	CurrencyHKD                     // 5 港币
+	CurrencyTWD                     // 6 台币
+	CurrencySGD                     // 7 新加坡元
+	CurrencyRUB                     // 8 卢布
+)
+
+// 货币符号映射
+var CurrencySymbols = map[Currency]string{
+	CurrencyCNY: "¥",
+	CurrencyUSD: "$",
+	CurrencyEUR: "€",
+	CurrencyGBP: "£",
+	CurrencyHKD: "HK$",
+	CurrencyTWD: "NT$",
+	CurrencySGD: "S$",
+	CurrencyRUB: "₽",
+}
